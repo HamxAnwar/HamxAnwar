806aff3058b85d74ea014b6ff20191af0bfec2b3
Hamza Anwar <114290075+HamxAnwar@users.noreply.github.com>
2023-06-11 14:38:32 +0500

    new profile

---
+++ script.js
@@ -0,0 +1,15 @@
+// Add any JavaScript code for interactive behavior or advanced animations here
+
+// Smooth scrolling when clicking on anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  // Add additional JavaScript code as needed for your website
+  
+  
