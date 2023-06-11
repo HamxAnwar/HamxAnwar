9fd18d13013e6794ffa57733b65032cd8912254d
Hamza Anwar <114290075+HamxAnwar@users.noreply.github.com>
2023-06-11 15:00:47 +0500

    test2

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-// Add any JavaScript code for interactive behavior or advanced animations here
-
-// Smooth scrolling when clicking on anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
+// Open the specified tab and show its content
+function openTab(event, tabId) {
+    const tabContents = document.getElementsByClassName('tabcontent');
+    for (let i = 0; i < tabContents.length; i++) {
+      tabContents[i].style.display = 'none';
+    }
   
-  // Add additional JavaScript code as needed for your website
+    const tabLinks = document.getElementsByClassName('tablinks');
+    for (let i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].classList.remove('active');
+    }
   
+    document.getElementById(tabId).style.display = 'block';
+    event.currentTarget.classList.add('active');
+  }
+  
+  // Set the default tab to be opened on page load
+  document.getElementsByClassName('tablinks')[0].click();
   
