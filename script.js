212a459e2a775f54ca7b82effb293edcfa7caa4a
Hamza Anwar <114290075+HamxAnwar@users.noreply.github.com>
2023-06-11 15:52:23 +0500

    aS

--- script.js
+++ script.js
@@ -1,78 +1,77 @@
 // Open the specified tab and show its content with sliding animation
 function openTab(event, tabId) {
-    const tabContents = document.getElementsByClassName('tabcontent');
-    const activeTab = document.getElementById(tabId);
-  
-    // Check if the tab is already active
-    if (activeTab.style.display === 'block') {
-      return;
-    }
-  
-    for (let i = 0; i < tabContents.length; i++) {
-      const tabContent = tabContents[i];
-  
-      if (tabContent.style.display === 'block') {
-        // Slide out the currently active tab
-        slideOut(tabContent);
-  
-        setTimeout(() => {
-          tabContent.style.display = 'none';
-  
-          // Slide in the selected tab
-          slideIn(activeTab);
-          activeTab.style.display = 'block';
-        }, 500); // Wait for slide-out animation to finish
-      }
-    }
-  
-    const tabLinks = document.getElementsByClassName('tablinks');
-    for (let i = 0; i < tabLinks.length; i++) {
-      tabLinks[i].classList.remove('active');
-    }
-  
-    event.currentTarget.classList.add('active');
+  const tabContents = document.getElementsByClassName('tabcontent');
+  const activeTab = document.getElementById(tabId);
+
+  // Check if the tab is already active
+  if (activeTab.style.display === 'block') {
+    return;
   }
-  
-  // Slide out the specified element
-  function slideOut(element) {
-    let pos = 0;
-    const id = setInterval(frame, 5);
-  
-    function frame() {
-      if (pos === -100) {
-        clearInterval(id);
-        element.style.display = 'none';
-      } else {
-        pos--;
-        element.style.right = pos + '%';
-      }
+
+  for (let i = 0; i < tabContents.length; i++) {
+    const tabContent = tabContents[i];
+
+    if (tabContent.style.display === 'block') {
+      // Slide out the currently active tab
+      slideOut(tabContent);
+
+      setTimeout(() => {
+        tabContent.style.display = 'none';
+
+        // Slide in the selected tab
+        slideIn(activeTab);
+        activeTab.style.display = 'block';
+      }, 500); // Wait for slide-out animation to finish
     }
   }
-  
-  // Slide in the specified element
-  function slideIn(element) {
-    let pos = -100;
-    element.style.display = 'block';
-    const id = setInterval(frame, 5);
-  
-    function frame() {
-      if (pos === 0) {
-        clearInterval(id);
-      } else {
-        pos++;
-        element.style.right = pos + '%';
-      }
+
+  const tabLinks = document.getElementsByClassName('tablinks');
+  for (let i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].classList.remove('active');
+  }
+
+  event.currentTarget.classList.add('active');
+}
+
+// Slide out the specified element
+function slideOut(element) {
+  let pos = 0;
+  const id = setInterval(frame, 5);
+
+  function frame() {
+    if (pos === -100) {
+      clearInterval(id);
+      element.style.display = 'none';
+    } else {
+      pos--;
+      element.style.right = pos + '%';
     }
   }
-  
-  // Add event listeners to the tab buttons
-  const tabButtons = document.getElementsByClassName('tablinks');
-  for (let i = 0; i < tabButtons.length; i++) {
-    tabButtons[i].addEventListener('click', function (event) {
-      openTab(event, this.getAttribute('data-tab'));
-    });
+}
+
+// Slide in the specified element
+function slideIn(element) {
+  let pos = -100;
+  element.style.display = 'block';
+  const id = setInterval(frame, 5);
+
+  function frame() {
+    if (pos === 0) {
+      clearInterval(id);
+    } else {
+      pos++;
+      element.style.right = pos + '%';
+    }
   }
-  
-  // Set the default tab to be opened on page load
-  document.getElementsByClassName('tablinks')[0].click();
-  
+}
+
+// Add event listeners to the tab buttons
+const tabButtons = document.getElementsByClassName('tablinks');
+for (let i = 0; i < tabButtons.length; i++) {
+  tabButtons[i].addEventListener('click', function (event) {
+    openTab(event, this.getAttribute('data-tab'));
+  });
+}
+
+// Set the default tab to be opened on page load
+document.getElementsByClassName('tablinks')[0].click();
